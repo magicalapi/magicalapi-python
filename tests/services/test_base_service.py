62b168525e211212099b179209e1e1987756747d
Amir Aref <amiraref808@gmail.com>
2024-01-24 10:41:41 +0330

    add tests for AsyncClient and Base Service

---
+++ tests/services/test_base_service.py
@@ -0,0 +1,44 @@
+import json
+import httpx
+import pytest
+import pytest_asyncio
+from magicalapi.services.base import BaseService
+from magicalapi.types.schemas import HttpResponse
+
+
+@pytest_asyncio.fixture(scope="function")
+async def httpxclient():
+    # base url is empty
+    client = httpx.AsyncClient(headers={"content-type": "application/json"})
+
+    yield client
+
+    await client.aclose()
+    del client
+
+
+@pytest.mark.asyncio
+async def test_base_service_post_request(httpxclient):
+    base_service = BaseService(httpxclient)
+    test_data = {"foo": "bar"}
+
+    response = await base_service._send_post_request(
+        path="https://httpbin.org/post", data=test_data
+    )
+    # check response type
+    assert isinstance(response, HttpResponse)
+    # check status code
+    assert response.status_code == 200
+    # check data transfered
+    assert json.loads(response.text)["json"] == test_data
+
+
+@pytest.mark.asyncio
+async def test_base_service_get_request(httpxclient):
+    base_service = BaseService(httpxclient)
+    response = await base_service._send_get_request(path="https://httpbin.org/get")
+
+    # check response type
+    assert isinstance(response, HttpResponse)
+    # check status code
+    assert response.status_code == 200
