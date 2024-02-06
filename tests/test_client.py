e98124407d5d085c3043f68d803b2298c4029cc9
Amir Aref <amiraref808@gmail.com>
2024-02-06 12:23:11 +0330

    fix client api_key tests and add .env test

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,6 +1,19 @@
+import os
 import pytest
+from magicalapi.settings import Settings, settings
+import magicalapi.client
 from magicalapi.client import AsyncClient
 from uuid import uuid4
+
+
+@pytest.fixture(scope="session", autouse=True)
+def api_key_env_var():
+
+    # override environment variables
+    _fake_api_key = str(uuid4())
+    os.environ["MAG_API_KEY"] = _fake_api_key
+
+    return os.environ["MAG_API_KEY"]
 
 
 @pytest.mark.asyncio
@@ -18,8 +31,19 @@ async def test_client_context_manager():
 
 
 def test_client_empty_api_key():
+    # ensure environment variables not set
+    settings.api_key = None
+
     with pytest.raises(TypeError):
         AsyncClient()
+
+
+def test_client_load_api_key_environment_variables(api_key_env_var: str):
+    # mock settings manager
+    # reload settings
+    magicalapi.client.settings = Settings()
+    client = AsyncClient()
+    assert client.api_key == api_key_env_var
 
 
 def test_client_invalid_api_key_type():
