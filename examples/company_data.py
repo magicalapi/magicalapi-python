43f26a1a04cec4700d3e5245f553aa44c32d3b55
Parisan Ahmadi <63796733+parisa-ahmadi@users.noreply.github.com>
2024-02-08 17:23:03 +0330

    Update and optimize company_data.py
    I used isinstance() function instead of type() comparison for checking the type of response.
    
    Added an output_file variable to store the name of the output JSON file.
    
    The entire main() function is wrapped in a try block, and any exceptions that occur during its execution are caught and handled in the except block. This approach provides a more robust error-handling mechanism compared to the previous version

--- examples/company_data.py
+++ examples/company_data.py
@@ -1,25 +1,31 @@
 import asyncio
 from magicalapi.client import AsyncClient
 from magicalapi.types.base import ErrorResponse
+import json
 
 API_KEY = "mag_123456789"
 company_name = "google"
+output_file = f"company_data_{company_name}.json"
 
 
 async def main():
-    async with AsyncClient(api_key=API_KEY) as client:
-        response = await client.company_data.get_company_data(company_name=company_name)
+    try:
+        async with AsyncClient(api_key=API_KEY) as client:
+            response = await client.company_data.get_company_data(company_name=company_name)
 
-        if type(response) == ErrorResponse:
-            # got error from api
-            print("Error :", response.message)
-        else:
-            # got response successfully
-            print("credists :", response.usage.credits)
-            print(response.data)
-            # save response in json file
-            with open(f"company_data_{company_name}.json", "w") as file:
-                file.write(response.model_dump_json(indent=3))
+            if isinstance(response, ErrorResponse):
+                # got error from api
+                print("Error:", response.message)
+            else:
+                # got response successfully
+                print("Credits:", response.usage.credits)
+                print(response.data)
+                # save response in json file
+                with open(output_file, "w") as file:
+                    json.dump(response.data, file, indent=3)
+
+    except Exception as e:
+        print("An error occurred:", str(e))
 
 
 asyncio.run(main())
