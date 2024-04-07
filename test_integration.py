09f35f4af1149518f6851902c4a349094ec5d69b
Amir Hasan Aref Asl <amiraref808@outlook.com>
2024-04-07 12:48:28 +0330

    test(integration-test): add test_integration.py script to test all services send request to server

---
+++ test_integration.py
@@ -0,0 +1,126 @@
+import pytest
+import pytest_asyncio
+
+from magicalapi.client import AsyncClient
+from magicalapi.types.company_data import CompanyDataResponse
+from magicalapi.types.profile_data import ProfileDataResponse
+from magicalapi.types.resume_parser import ResumeParserResponse
+from magicalapi.types.resume_review import ResumeReviewResponse
+from magicalapi.types.resume_score import ResumeScoreResponse
+from magicalapi.types.youtube_seo import YoutubeSeoResponse
+from magicalapi.types.youtube_suggestions import YoutubeSuggestionsResponse
+from magicalapi.types.youtube_top_keywords import YoutubeTopKeywordsResponse
+
+
+@pytest_asyncio.fixture(scope="function")
+async def client():
+    # create an instance of magicalAPI AsyncClient
+    async with AsyncClient() as client:  # loads api_key from .env
+        yield client
+
+
+# @pytest.mark.asyncio
+# async def test_video_not_found(client: AsyncClient):
+#     # test api returns 404 error for not found video
+#     response = await client.youtube_seo.get_youtube_seo(
+#         url=f"https://youtube.com/watch?v={uuid4()}"
+#     )
+
+#     assert all(
+#         (
+#             isinstance(response, ErrorResponse),
+#             response.status_code == 404,
+#         )
+#     )
+
+
+# @pytest.mark.asyncio
+# async def test_video_url_is_invalid(client: AsyncClient):
+#     # test api returns 400 error for invalid video url
+#     response = await client.youtube_seo.get_youtube_seo(url="https://example.com")
+
+#     assert all(
+#         (
+#             isinstance(response, ErrorResponse),
+#             response.status_code == 400,  # bad request
+#         )
+#     )
+
+
+@pytest.mark.asyncio
+async def test_profile_data_is_ok(client: AsyncClient):
+    # test api returns 200 and correct response schema
+    response = await client.profile_data.get_profile_data(profile_name="amiraref")
+
+    assert isinstance(response, ProfileDataResponse)
+
+
+@pytest.mark.asyncio
+async def test_company_data_is_ok(client: AsyncClient):
+    # test api returns 200 and correct response schema
+    response = await client.company_data.get_company_data(company_name="google")
+
+    assert isinstance(response, CompanyDataResponse)
+
+
+@pytest.mark.asyncio
+async def test_resume_parser_is_ok(client: AsyncClient):
+    # test api returns 200 and correct response schema
+    response = await client.resume_parser.get_resume_parser(
+        url="https://resume-resource.com/wp-content/uploads/00123-sales-professional-resume.pdf"
+    )
+
+    assert isinstance(response, ResumeParserResponse)
+
+
+@pytest.mark.asyncio
+async def test_resume_review_is_ok(client: AsyncClient):
+    # test api returns 200 and correct response schema
+    response = await client.resume_review.get_resume_review(
+        url="https://resume-resource.com/wp-content/uploads/00123-sales-professional-resume.pdf"
+    )
+
+    assert isinstance(response, ResumeReviewResponse)
+
+
+@pytest.mark.asyncio
+async def test_resume_score_is_ok(client: AsyncClient):
+    # test api returns 200 and correct response schema
+    response = await client.resume_score.get_resume_score(
+        url="https://resume-resource.com/wp-content/uploads/00123-sales-professional-resume.pdf",
+        job_description="Sales Professional",
+    )
+
+    assert isinstance(response, ResumeScoreResponse)
+
+
+@pytest.mark.asyncio
+async def test_youtube_seo_is_ok(client: AsyncClient):
+    # test api returns 200 and correct response schema
+    response = await client.youtube_seo.get_youtube_seo(
+        url="https://www.youtube.com/watch?v=PZZI1QXlM80"
+    )
+
+    assert isinstance(response, YoutubeSeoResponse)
+
+
+@pytest.mark.asyncio
+async def test_youtube_suggestions_is_ok(client: AsyncClient):
+    # test api returns 200 and correct response schema
+    response = await client.youtube_suggestions.get_youtube_suggestions(
+        prompt_sentence="How to create a profitable Shopify store",
+        count=5,
+        suggestion_goal="hashtag",
+    )
+
+    assert isinstance(response, YoutubeSuggestionsResponse)
+
+
+@pytest.mark.asyncio
+async def test_youtube_keywords_is_ok(client: AsyncClient):
+    # test api returns 200 and correct response schema
+    response = await client.youtube_top_keywords.get_keywords(
+        search_sentence="movie trailers", country="1", language="1000"
+    )
+
+    assert isinstance(response, YoutubeTopKeywordsResponse)
